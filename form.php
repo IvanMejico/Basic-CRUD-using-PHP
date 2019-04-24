c3ffe1277638f66ca6667f69581c3c14acc967a8
IvanMejico <ivan.mejico21@gmail.com>
2019-04-24 16:08:44 +0800

    initial commit

---
+++ form.php
@@ -0,0 +1,23 @@
+<div>
+			<form action="POST">
+				<select name="course">
+					<option value="bscpe">BSCpE</option>
+					<option value="bsce">BSCE</option>
+					<option value="bsme">BSME</option>
+					<option value="bsme">BSEE</option>
+					<option value="bsece">BSECE</option>
+				</select>
+
+				<p><input type="studentid" placeholder="Student ID"></p>
+				<p><input type="firstname" placeholder="First Name"></p>
+				<p><input type="middlename" placeholder="Middle Name"></p>
+				<p><input type="lastname" placeholder="Last Name"><br></p>
+
+
+				<input type="radio" name="gender" value="male">Male
+				<input type="radio" name="gender" value="female">Female
+				<input type="radio" name="gender" value="other">Other
+
+				<p><input type="submit" value="Submit"></p>
+			</form>
+		</div>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Lab Activity</title>
+		<link rel="stylesheet" href="">
+	</head>
+	<body>
+		<?php include("form.php")?>
+		<?php /*include("table.php")*/ ?>
+	</body>
+</html>
